a0f71e5c0b9cc92c63d3de43da2f0ca5b44bcf06
CharlesTBetz <char@erp4it.com>
2015-02-22 12:22:46 -0600

    Uncommented remote server interaction in master

--- cookbooks/manos/recipes/default.rb
+++ cookbooks/manos/recipes/default.rb
@@ -101,35 +101,35 @@ execute 'initialize git 5' do
   command 'git commit -m "initial commit"'   # needs to be idempotent
 end
 
-#execute 'register server' do
-#  user "vagrant"
-#  group "git"
-#  cwd '/home/hijo'
-#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
-#  command 'ssh-keyscan cerebro >> ~/.ssh/known_hosts'   # prevents interactive dialog
-#end
-#
-#execute 'define remote' do
-#  user "vagrant"
-#  cwd '/home/hijo'
-#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
-#  command 'git remote add origin ssh://cerebro/home/hijo.git'   # define master git server. high priority to make idempotent.
-#end
-#
-#
-#execute 'push to remote' do
-#  user "vagrant"
-#  cwd '/home/hijo'
-#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
-#  command 'git push origin --mirror'   # erase master - if rebuilding manos, assume this is desired. may want to reconsider.
-#end
-#
-#execute 'push to remote' do
-#  user "vagrant"
-#  cwd '/home/hijo'
-#  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
-#  command 'git push origin master'   # push to master git server
-#end
+execute 'register server' do
+  user "vagrant"
+  group "git"
+  cwd '/home/hijo'
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
+  command 'ssh-keyscan cerebro >> ~/.ssh/known_hosts'   # prevents interactive dialog
+end
+
+execute 'define remote' do
+  user "vagrant"
+  cwd '/home/hijo'
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
+  command 'git remote add origin ssh://cerebro/home/hijo.git'   # define master git server. high priority to make idempotent.
+end
+
+
+execute 'push to remote' do
+  user "vagrant"
+  cwd '/home/hijo'
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
+  command 'git push origin --mirror'   # erase master - if rebuilding manos, assume this is desired. may want to reconsider.
+end
+
+execute 'push to remote' do
+  user "vagrant"
+  cwd '/home/hijo'
+  environment ({'HOME' => '/home/vagrant', 'USER' => 'vagrant'})  
+  command 'git push origin master'   # push to master git server
+end
 
 
 
