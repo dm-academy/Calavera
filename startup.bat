c9e573fbdff2294bac25fa4b35b0f7e1b66beb9b
Charles T. Betz <char@erp4it.com>
2015-04-06 13:31:32 -0600

    fixed #12 rm in win bat script

--- startup.bat
+++ startup.bat
@@ -2,9 +2,9 @@ REM Windows shell script to initialize base box
 REM not sure how/if this runs as PowerShell
 
 vagrant destroy base -f
-rm -f package.box
+del package.box
 vagrant up base
 vagrant package base
 vagrant box add opscode-ubuntu-14.04a package.box -f
-rm -f package.box
+del package.box
 vagrant destroy base -f
