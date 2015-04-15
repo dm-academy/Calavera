7cd9893a9cf6a921efed01436a4b5de0c005cda6
Liora Milbaum <liora@lmb.co.il>
2015-04-15 09:28:31 +0300

    adding apt-get update and installing tree via apt_package

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,13 +1,10 @@
 # base configuration.
 # this needs to be then re-packaged to minimize virtual machine loading time while constructing the various nodes
 
-#execute 'apt update' do
-#  command 'apt-get -q update'   
-#end
-
+include_recipe "apt::default"
 include_recipe "java7::default"
 include_recipe "curl::default"
 
-execute 'install tree' do
-  command 'apt-get -q install tree'   
+apt_package 'tree' do
+  action :install
 end
