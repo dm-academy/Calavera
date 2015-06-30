2bf7a5860699b14f16aa8c915407b53dc700544d
Stuart Charlton <scharlton@pivotal.io>
2015-06-30 17:01:45 -0400

    Upgrade ANT to 1.9.5

--- cookbooks/localAnt/recipes/default.rb
+++ cookbooks/localAnt/recipes/default.rb
@@ -12,22 +12,22 @@
 # downloading and reinstallilng is not that costly
 
 #execute echo "installing Ant"
-remote_file "/opt/apache-ant-1.9.4-bin.tar.gz" do
-    source 'http://mirror.nexcess.net/apache//ant/binaries/apache-ant-1.9.4-bin.tar.gz'
+remote_file "/opt/apache-ant-1.9.5-bin.tar.gz" do
+    source 'http://mirror.nexcess.net/apache//ant/binaries/apache-ant-1.9.5-bin.tar.gz'
 end
 
 bash 'install Ant' do
   cwd '/opt'
   code <<-EOH
-    tar xzf apache-ant-1.9.4-bin.tar.gz
-    rm -f apache-ant-1.9.4-bin.tar.gz
-    echo export ANT_HOME=/opt/apache-ant-1.9.4 > /etc/profile.d/ant.sh  # not idempotent, will continue adding repeated lines
-    echo export ANT_HOME=/usr/share/apache-ant-1.9.4 >> ~/.bashrc       # need to write a simple grep test
+    tar xzf apache-ant-1.9.5-bin.tar.gz
+    rm -f apache-ant-1.9.5-bin.tar.gz
+    echo export ANT_HOME=/opt/apache-ant-1.9.5 > /etc/profile.d/ant.sh  # not idempotent, will continue adding repeated lines
+    echo export ANT_HOME=/usr/share/apache-ant-1.9.5 >> ~/.bashrc       # need to write a simple grep test
     mkdir -p /home/jenkins
-    echo export ANT_HOME=/usr/share/apache-ant-1.9.4 >> /home/jenkins/.bashrc
+    echo export ANT_HOME=/usr/share/apache-ant-1.9.5 >> /home/jenkins/.bashrc
     EOH
 end
 
 link "/usr/local/bin/ant" do
-  to "/opt/apache-ant-1.9.4/bin/ant"
+  to "/opt/apache-ant-1.9.5/bin/ant"
 end
