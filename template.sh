90e11d1cc0902184f712b283ae1e7f1fb80bc536
CharlesTBetz <char@erp4it.com>
2015-10-04 16:49:49 -0500

    vagrant template

---
+++ template.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# this script sets up a new pipeline from the template, with environment naming as set on command line
+# don't like how it edits the resources directly, would be better handled as a parameter across all the
+# Chef recipes
+# needs to be run from Calavera dir as uses relative paths, yuck
+
+mv Vagrantfile oldVagrantfile
+cp VagrantfileTemplate Vagrantfile
+sed -i -- 's/xx/$1/g' Vagrantfile
+
+# fix various resources in Chef
