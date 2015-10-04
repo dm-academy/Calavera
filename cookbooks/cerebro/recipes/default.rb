090ccb1f30fb8b73c5fe20cd3f513d0ec41e091b
Charles T. Betz <char@erp4it.com>
2015-10-04 13:05:19 -0500

    backing out of 5 pipelines

--- cookbooks/cerebro/recipes/default.rb
+++ cookbooks/cerebro/recipes/default.rb
@@ -17,14 +17,6 @@ user 'jenkins' do   # jenkins will need to ssh in to retrieve files to build
   group 'git'
 end
 
-directory "/home/hijo.git/"  do
-    mode 00775
-    owner "git"
-    group "git"
-    action :create
-    recursive true
-end
-
 directory "/home/jenkins/.ssh"  do
     mode 00700      # this will fail with other permissions
     owner "jenkins"
@@ -41,6 +33,16 @@ end
 execute 'correct Jenkins ssh files ownership' do
   command 'chown -R jenkins /home/jenkins &&  \
           chgrp -R jenkins /home/jenkins'
+end
+
+### hijo config
+
+directory "/home/hijo.git/"  do
+    mode 00775
+    owner "git"
+    group "git"
+    action :create
+    recursive true
 end
 
 execute 'init git' do
@@ -60,6 +62,39 @@ end
 
 cookbook_file "post-receive" do
     path "/home/hijo.git/hooks/post-receive"
+    user "jenkins"
+    group "jenkins"
+    mode 00755    # must be executable
+    action :create
+end
+
+### gatito config
+
+directory "/home/gatito.git/"  do
+    mode 00775
+    owner "git"
+    group "git"
+    action :create
+    recursive true
+end
+
+execute 'init git' do
+  user "git"
+  group "git"
+  command 'git init --bare --shared=group /home/gatito.git'
+end
+
+execute 'init git' do
+  user "git"
+  group "git"
+  cwd '/home/gatito.git'
+  command "git config receive.denynonfastforwards false"    # this way we can force wipe from manos if manos is rebuilt after cerebro
+end
+#configure post receive hook
+# that means manos is dependent on havng cerebro done in terms of ordering
+
+cookbook_file "post-receive-gatito" do
+    path "/home/gatito.git/hooks/post-receive"
     user "jenkins"
     group "jenkins"
     mode 00755    # must be executable
