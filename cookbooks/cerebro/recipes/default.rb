123902cd01402de8c5e44bad4944e2922b6e1523
Charles T. Betz <char@erp4it.com>
2015-10-05 13:51:46 -0500

    cerebro ssh fix for templated version

--- cookbooks/cerebro/recipes/default.rb
+++ cookbooks/cerebro/recipes/default.rb
@@ -26,8 +26,10 @@ directory "/home/jenkins/.ssh"  do
 end
 
 execute 'Jenkins keys' do
-  cwd 'home/vagrant/.ssh'
-  command 'cp * /home/jenkins/.ssh'  # this should include authorized keys.
+  #cwd 'home/vagrant/.ssh' # somehow out of synch in templated version, suspect startup.sh issue
+  #command 'cp * /home/jenkins/.ssh'  # this should include authorized keys.
+  command 'cp /mnt/shared/keys/* /home/jenkins/.ssh'  # this should be the source - fixes part of problem
+  commant 'cat /home/jenkins/.ssh/id_rsa.pub > /home/jenkins/.ssh/authorized_keys'
 end
 
 execute 'correct Jenkins ssh files ownership' do
