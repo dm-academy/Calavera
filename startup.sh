c73b7bc503fcea1892dd1c97c2120838cde6bc29
Charles T. Betz <char@erp4it.com>
2015-10-11 09:59:22 -0500

    startup fix

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ vagrant plugin install vagrant-berkshelf
 vagrant destroy base -f  # insert guard
 rm -f package.box
 rm -f ./shared/keys/*   # re-set public/private keys
-#vagrant box add http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box --name opscode/temp --force
+vagrant box add http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box --name opscode/temp --force
 
 vagrant up base
 vagrant package base
