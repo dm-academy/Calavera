173a11b0e26f0e03937062921559ff2e8f907d14
CharlesTBetz <char@erp4it.com>
2015-11-07 22:17:00 -0600

    supporting itop with new pies srv

--- cookbooks/pies/files/pies.sh
+++ cookbooks/pies/files/pies.sh
@@ -13,3 +13,12 @@ cd /var/www
 sudo wget http://downloads.sourceforge.net/project/itop/itop/2.2.0/iTop-2.2.0-2459.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fitop%2Ffiles%2F&ts=1446939722&use_mirror=tcpdiag iTop-2.2.0-2459.zip
 
 sudo mv iTop-* iTop.zip
+sudo unzip iTop.zip -d itop
+
+sudo chmod 777 /etc/apache2/000-default.conf
+sudo sed -i -- "s/html/itop/" /etc/apache2/ 000-default.conf
+sudo chmod 744 /etc/apache2/000-default.conf
+
+sudo chown -R www-data itop
+
+sudo service apache2 restart
